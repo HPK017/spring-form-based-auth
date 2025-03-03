82ee5bb6c13cdcd0e6d70ef3c65e98be4f4c9bdb
HPK017 <parelharsh0250@gmail.com>
2025-03-03 15:18:21 +0530

    first commit

---
+++ src/main/java/com/example/spring_form_based_auth/SpringFormBasedAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_form_based_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFormBasedAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFormBasedAuthApplication.class, args);
+	}
+
+}
